2a1094f5bde1792352d2ccbd862e264abe2a1eca
jv640 <vermajatin640@gmail.com>
2020-07-11 12:45:29 +0530

    made changes about Match_id and to decide home team and away team

--- IPL matches data/try.py
+++ IPL matches data/try.py
@@ -2,7 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 import html5lib as h5l
 count = 1
-while 1:
-    r = requests.get("http://www.hotstar.com")
-    print(count)
-    count = count + 1
+
+r = requests.get("https://www.iplt20.com/archive/2018/01")
+print(r.history)
+htmlContent = r.content
+soup = BeautifulSoup(htmlContent, 'html.parser')
+
